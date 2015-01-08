b2e5c94933cdc388617a8041a21d2c73a0814afd
RichardPurvis <steelthunderfalcon@gmail.com>
2015-01-08 16:41:40 -0500

    comments update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,10 +3,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comments = @post.comments
-
-    # @comment = current_user.comments.build(comment_params)
-    # @comment.post = @post
-
+    
     @comment = current_user.comments.build(comment_params)
     @comment.post = @post
 
