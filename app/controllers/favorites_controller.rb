3f110ab22c4edd17aeaa79558a17738acaa91ecb
RichardPurvis <steelthunderfalcon@gmail.com>
2015-02-01 16:26:32 -0500

    Finished favorites assignment - kind of.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,27 @@
 class FavoritesController < ApplicationController
+
+  def  create
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.build(post: @post)
+    authorize favorite
+    if favorite.save
+      flash[:notice] = "This post is now a FAVORITE POST!!!"
+      redirect_to [@post.topic, @post]
+    else
+      flash[:error] = "ERROR: no matter how much you want to, you cannot favorite this post."
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find(params[:id])
+    authorize favorite
+    if favorite.destroy
+      flash[:notice] = "You hate this post now :)"
+      redirect_to [@post.topic, @post]
+    else
+      flash[:error] = "ERROR: Somthing went wrong and this is still your favorite post."
+      redirect_to [@post.topic, @post]
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,19 @@ describe User do
   include TestFactories
 
   describe "#favorited(post)" do
-    xit "returns 'nil' if the user has not favorited the post" do
+#I have no idea how to start this one. PLEASE HELP! Do I have to make a user create a new post?
+    before do
+      @post = associated_post
+      @user = authenticated_user
+
     end
 
-    xit "returns the appropriate favorite if it exists" do
+    it "returns 'nil' if the user has not favorited the post" do
+      expect( @user.favorited ).to eq(nil)
+    end
+
+    it "returns the appropriate favorite if it exists" do
+      expect( @user.post.favorite ).to eq()
     end
   end
 end
