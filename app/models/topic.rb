7164b499b5ccbc769d103bae341cfe2972c6b879
RichardPurvis <steelthunderfalcon@gmail.com>
2015-02-05 18:12:27 -0500

    specs and scoping

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,8 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+
+    scope :visible_to, -> (user) { user ? all : where(public: true) }
+    #                            If user? Then: 'all', else: 'where(public: true)'
+
+    scope :publicly_viewable, -> { where(public: true)}
 end
