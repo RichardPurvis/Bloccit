bfd9a7fed1c9f53a3214fc9794ad3b44b20380ac
RichardPurvis <steelthunderfalcon@gmail.com>
2015-02-08 10:52:50 -0500

    Removed testfactory and installed FactoryGirl AND modified my specs to work!

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'rails', '4.1.7'
 group :test do
   gem 'rspec-rails', '~> 3.1.0'
   gem 'capybara'
+  gem 'database_cleaner'
+  gem 'factory_girl_rails', '~> 4.0'
 end
 
 # Use SCSS for stylesheets
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 describe User do
 
-  include TestFactories
 
   describe "#favorited(post)" do
 #I have no idea how to start this one. PLEASE HELP! Do I have to make a user create a new post?
     before do
-      @post = associated_post
-      @user = authenticated_user
+      @post = create(:post)
+      @user = create(:user)
 
     end
 
@@ -18,6 +17,34 @@ describe User do
 
     it "returns the appropriate favorite if it exists" do
       expect( @user.post.favorite ).to eq()
+    end
+  end
+
+  describe ".top_rated" do
+
+    before do
+      @user1 = create(:user)
+      post = create(:post, user: @user1)
+      create(:comment, user: @user1, post: @post1)
+
+      @user2 = create(:user)
+      post = create(:post, user: @user1)
+      2.times { create(:comment, user: @user1, post: @post1) }
+
+    end
+
+    it "returns users ordered by comment + posts" do
+      expect( User.top_rated ).to eq([@user2, @user1])
+    end
+
+    it "stores a 'posts_count' on user" do
+      users = User.top_rated
+      expect( users.first.posts_count ).to eq(1)
+    end
+
+    it "stores a 'comments_count' on user" do
+      users = User.top_rated
+      expect( users.first.comments_count ).to eq(2)
     end
   end
 end
